fb4cfdf79c0f45572406ba8e4fd8b0c9537ea252
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-09 23:30:59 -0400

    created pid_controller.h implementation in pid_controller.cpp

--- libs/libcontroller/src/pid_controller.cpp
+++ libs/libcontroller/src/pid_controller.cpp
@@ -1,6 +1,9 @@
 #include "pid_controller.h"
+#include <set>
 
 using controller::PIDController;
+
+// write implementation here
 
 PIDController::PIDController(double kp, double kd, double ki, 
             double out_min, double out_max, double sampling_rate) :
@@ -11,5 +14,28 @@ PIDController::PIDController(double kp, double kd, double ki,
 
 double PIDController::compute(double setpoint, double measurement) {
         
-        return 3.14;
+        // calculate the error 
+        double error = setpoint - measurement;
+        // compute proprtional control
+        double prop_ctrl_ = kp_ * error;
+
+        // calculate the integral error
+        integral_error_ += error * dt_;
+        double integral_ctrl_ = ki_ * integral_error_;
+
+        // compute the derivative error
+        double derivative_error_ = (error - prev_error_)/dt_;
+        double derivative_ctrl_ = kd_ * derivative_error_;
+
+        prev_error_ = error;
+        // sum the p-i-d controls
+        double output = prop_ctrl_ + integral_ctrl_ + derivative_ctrl_;
+
+        // clamp output to bounds
+        if (output > out_max_)
+                return out_max_;
+        else if (output < out_min_)
+                return out_min_;
+        else
+                return output;
 }
