1cb776b3803428a5f9815f750a75f16ac48c67a4
FabrizzioCoronado <fabrizzio37@gmail.com>
2023-10-10 13:20:15 -0400

    formatted and eliminated bugs using cppcheck and cpplint. Added outputs to results folder

--- app/main.cpp
+++ app/main.cpp
@@ -3,16 +3,13 @@
 // take input from user and show output here
 
 int main() {
-
   controller::PIDController myController(0.15, .25, .20, 0, 70, .02);
   double setPoint = 45;
   double actualOutput = 20;
-  double output;
 
-  for (int i = 0; i < 30; i++){
-    output = myController.compute(setPoint, actualOutput);
+  for (int i = 0; i < 30; i++) {
+    double output = myController.compute(setPoint, actualOutput);
     actualOutput += output;
   }
   std::cout << "PID output is: " << actualOutput << std::endl;
-
 }
--- libs/libcontroller/src/pid_controller.cpp
+++ libs/libcontroller/src/pid_controller.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "pid_controller.h"
+
 #include <set>
 
 using controller::PIDController;
@@ -17,42 +18,44 @@ using controller::PIDController;
  * @param out_max Maximum output
  * @param sampling_rate Calculation frequency
  */
-PIDController::PIDController(double kp, double kd, double ki, 
-            double out_min, double out_max, double sampling_rate) :
-            kp_(kp), kd_(kd), ki_(ki), 
-            out_max_(out_max), out_min_(out_min),
-            dt_(sampling_rate) {}
-       
+PIDController::PIDController(double kp, double kd, double ki, double out_min,
+                             double out_max, double sampling_rate)
+    : kp_(kp),
+      kd_(kd),
+      ki_(ki),
+      out_max_(out_max),
+      out_min_(out_min),
+      dt_(sampling_rate) {}
+
 /**
- * @brief Compute method to calculate PID control output based on setpoint and measured velocity.
+ * @brief Compute method used to calculate the PID control output
  * @param setpoint Desired setpoint value
  * @param measurement Input actual velocity value
  * @return PID control output
  */
 double PIDController::compute(double setpoint, double measurement) {
-        
-        // calculate the error 
-        double error = setpoint - measurement;
-        // compute proprtional control
-        double prop_ctrl_ = kp_ * error;
+  // calculate the error
+  double error = setpoint - measurement;
+  // compute proprtional control
+  double prop_ctrl_ = kp_ * error;
 
-        // calculate the integral error
-        integral_error_ += error * dt_;
-        double integral_ctrl_ = ki_ * integral_error_;
+  // calculate the integral error
+  integral_error_ += error * dt_;
+  double integral_ctrl_ = ki_ * integral_error_;
 
-        // compute the derivative error
-        double derivative_error_ = (error - prev_error_)/dt_;
-        double derivative_ctrl_ = kd_ * derivative_error_;
+  // compute the derivative error
+  double derivative_error_ = (error - prev_error_) / dt_;
+  double derivative_ctrl_ = kd_ * derivative_error_;
 
-        prev_error_ = error;
-        // sum the p-i-d controls
-        double output = prop_ctrl_ + integral_ctrl_ + derivative_ctrl_;
+  prev_error_ = error;
+  // sum the p-i-d controls
+  double output = prop_ctrl_ + integral_ctrl_ + derivative_ctrl_;
 
-        // clamp output to bounds
-        if (output > out_max_)
-                return out_max_;
-        else if (output < out_min_)
-                return out_min_;
-        else
-                return output;
+  // clamp output to bounds
+  if (output > out_max_)
+    return out_max_;
+  else if (output < out_min_)
+    return out_min_;
+  else
+    return output;
 }
